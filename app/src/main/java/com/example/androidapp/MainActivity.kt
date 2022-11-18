6772db38d87dac29a36ee49f7f25bb3f079e98cf
henrylillvis <henry.lillvis@gmail.com>
2022-11-18 19:27:13 +0200

    add components to controlfragment

--- app/src/main/java/com/example/androidapp/MainActivity.kt
+++ app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -14,5 +14,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-    }
 }
+    }
