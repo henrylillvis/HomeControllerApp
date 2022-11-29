5fd1fa4379f5f80518dc93685b5973a9da0b7d6c
jere <jere.karkkainen@outlook.com>
2022-11-29 14:56:53 +0200

    Changes to API

--- app/src/main/java/com/example/androidapp/screens/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/androidapp/screens/settings/SettingsFragment.kt
@@ -6,8 +6,10 @@ import android.util.Log.d
 import androidx.appcompat.app.AppCompatDelegate
 import com.example.androidapp.R
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.preference.*
 import com.example.androidapp.network.HomeApi
+import kotlinx.coroutines.launch
 
 
 /**
@@ -21,17 +23,30 @@ class SettingsFragment : PreferenceFragmentCompat() {
         val nameSignature = findPreference<EditTextPreference>("signature")
         val homeSignature = findPreference<EditTextPreference>("home_signature")
         val serverSignature = findPreference<EditTextPreference>("server_signature")
+        val pirSignature = findPreference<EditTextPreference>("pir_signature")
         val theme = findPreference<SwitchPreferenceCompat>("theme")
         val apiVM: HomeApi by activityViewModels()
 
-        // force ipv4 address and set correct address to apiViewModel
+        // force ipv4 address + port and set to apiViewModel
         serverSignature?.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference, newValue ->
-            if (newValue.toString().matches("""^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(\.(?!$)|$)){4}$""".toRegex())) {
+            if (newValue.toString().matches("""^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]):[0-9]+${'$'}""".toRegex())) {
                 apiVM._address.value = newValue.toString()
                 true
             } else {
                 false
             }
+        }
+
+        pirSignature?.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference, newValue ->
+            if (newValue.toString().matches("""^[]?\d+${'$'}""".toRegex())) {
+                val seconds = newValue.toString().toInt()
+                lifecycleScope.launch {
+                    if (apiVM.service?.setPir(seconds)!!.seconds == seconds){
+                        true
+                    }
+                }
+            }
+            false
         }
 
         theme?.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference, newValue ->
