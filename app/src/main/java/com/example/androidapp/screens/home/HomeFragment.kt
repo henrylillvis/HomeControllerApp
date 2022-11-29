c79ff76f4c659c4c1e6b42843d6ae61ec2887f63
J-Bloh <82229826+J-Bloh@users.noreply.github.com>
2022-11-29 11:16:59 +0200

    add api functionality and refresh button

--- app/src/main/java/com/example/androidapp/screens/home/HomeFragment.kt
+++ app/src/main/java/com/example/androidapp/screens/home/HomeFragment.kt
@@ -7,7 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import androidx.preference.PreferenceManager
 import com.example.androidapp.HomeController
@@ -15,25 +15,36 @@ import com.example.androidapp.R
 import com.example.androidapp.databinding.FragmentHomeBinding
 import com.example.androidapp.network.StatesProperty
 import com.example.androidapp.network.WeatherProperty
-
+import androidx.lifecycle.*
+import com.example.androidapp.network.HomeApi
+import kotlinx.coroutines.*
+import java.lang.Exception
 
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
-    var user = ""
-    var home = ""
-    lateinit var weather: WeatherProperty
-    lateinit var states: StatesProperty
+    private var home = ""
+    private var weather: WeatherProperty? = null
+    private var states: StatesProperty? = null
+    private val apiVM: HomeApi by activityViewModels()
+    private var searching = false
+
+    private val _response = MutableLiveData<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater,
-            R.layout.fragment_home,container,false)
+        binding = FragmentHomeBinding.inflate(inflater)
+        //binding.lifecycleOwner = this
 
-        d("jb","Näkymä ajetaan")
+        binding.refreshButton.setOnClickListener{
+            if(!searching){
+                hideData()
+                getData()
+            }
+        }
         /**
          * Navigointi
          */
@@ -46,48 +57,74 @@ class HomeFragment : Fragment() {
         binding.logButton.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_homeFragment_to_logDataFragment)
         }
-        updateTemperatures()
-        updateStates()
-        setUser()
-        getHome()
         setHome()
-        setTemperatures()
-        setRelayStates()
+        getData()
         return binding.root
     }
-    private fun setHome() {
-        binding.homeSign.text = home
 
+    private fun getData() {
+        d("jb","getData started")
+        lifecycleScope.launch {
+            searching = true
+            binding.progressbar.visibility = View.VISIBLE
+            try {
+
+                //Service ei kerkeä latautumaan ensimmäisellä kerralla?
+                if(apiVM.service == null){
+                    d("jb",apiVM.service.toString())
+                    delay(1000)
+                    if (apiVM.service == null){throw Exception("Virhe yhteyden muodostamisessa.")}
+                }
+
+                weather = apiVM.service?.getWeather()
+                states = apiVM.service?.getStates()
+                _response.value = "Haku onnistui"
+                setTemperatures()
+                setRelayStates()
+                showData()
+            }catch (e: Exception){
+                val errorMsg = "Haku epäonnistui: ${e.message}"
+                _response.value = errorMsg
+                showError(errorMsg)
+            }
+            binding.progressbar.visibility = View.GONE
+            d("jb","Api response: "+_response.value )
+            searching = false
+        }
     }
+    private fun showData(){
+        binding.inTempView.visibility = View.VISIBLE
+        binding.outTempView.visibility = View.VISIBLE
+        binding.relayView.visibility = View.VISIBLE
+    }
+    private fun hideData(){
+        binding.errorCard.visibility = View.GONE
+        binding.inTempView.visibility = View.GONE
+        binding.outTempView.visibility = View.GONE
+        binding.relayView.visibility = View.GONE
+    }
+
+    private fun showError(msg: String?){
+        binding.errorMessage.text = msg
+        binding.errorCard.visibility = View.VISIBLE
+    }
+
     private fun setTemperatures() {
-        binding.inTemp.text = weather.temp2.toString() + " °C"
-        binding.outTemp.text = weather.temp1.toString() + " °C"
-        binding.inHum.text = weather.humid.toString() + " %"
+        binding.inTemp.text = weather?.temp2.toString() + " °C"
+        binding.outTemp.text = weather?.temp1.toString() + " °C"
+        binding.inHum.text = weather?.humid.toString() + " %"
     }
+
     private fun setRelayStates(){
-        binding.relay1State.text = states.relayOne
-        binding.relay2State.text = states.relayTwo
-        binding.relay3State.text = states.relayThree
-        binding.relay4State.text = states.relayFour
+        binding.relay1State.text = states?.relayOne
+        binding.relay2State.text = states?.relayTwo
+        binding.relay3State.text = states?.relayThree
+        binding.relay4State.text = states?.relayFour
     }
 
-    private fun updateTemperatures() {
-        weather = WeatherProperty(-5.2,23.2,12.5)
-    }
-
-    private fun updateStates() {
-        states = StatesProperty("AUTO","ON","OFF","ON")
-    }
-
-    private fun setUser() {
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(HomeController.appContext)
-        user = sharedPreferences.getString("signature", "").toString()
-    }
-
-    private fun getHome() {
+    private fun setHome() {
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(HomeController.appContext)
         home = sharedPreferences.getString("home_signature", "").toString()
-        d("jb", home)
+        binding.homeSign.text = home
     }
-
 }
