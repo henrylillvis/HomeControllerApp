56d61afadff2911b5d37651748081a28fdbdaddb
henrylillvis <henry.lillvis@gmail.com>
2022-12-03 00:22:18 +0200

    Add simple camera usage to app

--- app/src/main/java/com/example/androidapp/screens/home/HomeFragment.kt
+++ app/src/main/java/com/example/androidapp/screens/home/HomeFragment.kt
@@ -1,12 +1,31 @@
 package com.example.androidapp.screens.home
 
 
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log.d
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import androidx.preference.PreferenceManager
@@ -18,8 +37,11 @@ import com.example.androidapp.network.WeatherProperty
 import androidx.lifecycle.*
 import com.example.androidapp.network.HomeApi
 import kotlinx.coroutines.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 import java.lang.Exception
-
 
 class HomeFragment : Fragment() {
 
@@ -29,8 +51,16 @@ class HomeFragment : Fragment() {
     private var states: StatesProperty? = null
     private val apiVM: HomeApi by activityViewModels()
     private var searching = false
-
     private val _response = MutableLiveData<String>()
+
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 1
+        private const val CAMERA_REQUEST_CODE = 2
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,10 +69,27 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(inflater)
         //binding.lifecycleOwner = this
 
-        binding.refreshButton.setOnClickListener{
-            if(!searching){
+        binding.refreshButton.setOnClickListener {
+            if (!searching) {
                 hideData()
                 getData()
+            }
+        }
+        binding.cameraButton.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    android.Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                //camera intent
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            } else {
+                ActivityCompat.requestPermissions(
+                    requireContext() as Activity,
+                    arrayOf(android.Manifest.permission.CAMERA),
+                    CAMERA_PERMISSION_CODE
+                )
             }
         }
         /**
@@ -62,18 +109,32 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        try {
+            if (requestCode == CAMERA_REQUEST_CODE) {
+                val photo = data!!.extras!!["data"] as Bitmap?
+                binding.homeImage.setImageBitmap(photo)
+            }
+        } catch (e: Exception) {
+            Toast.makeText(context, "${e.message}", Toast.LENGTH_LONG).show()
+        }
+    }
+
     private fun getData() {
-        d("jb","getData started")
+        d("jb", "getData started")
         lifecycleScope.launch {
             searching = true
             binding.progressbar.visibility = View.VISIBLE
             try {
 
                 //Service ei kerkeä latautumaan ensimmäisellä kerralla?
-                if(apiVM.service == null){
-                    d("jb",apiVM.service.toString())
+                if (apiVM.service == null) {
+                    d("jb", apiVM.service.toString())
                     delay(1000)
-                    if (apiVM.service == null){throw Exception("Virhe yhteyden muodostamisessa.")}
+                    if (apiVM.service == null) {
+                        throw Exception("Virhe yhteyden muodostamisessa.")
+                    }
                 }
 
                 weather = apiVM.service?.getWeather()
@@ -82,29 +143,31 @@ class HomeFragment : Fragment() {
                 setTemperatures()
                 setRelayStates()
                 showData()
-            }catch (e: Exception){
+            } catch (e: Exception) {
                 val errorMsg = "Haku epäonnistui: ${e.message}"
                 _response.value = errorMsg
                 showError(errorMsg)
             }
             binding.progressbar.visibility = View.GONE
-            d("jb","Api response: "+_response.value )
+            d("jb", "Api response: " + _response.value)
             searching = false
         }
     }
-    private fun showData(){
+
+    private fun showData() {
         binding.inTempView.visibility = View.VISIBLE
         binding.outTempView.visibility = View.VISIBLE
         binding.relayView.visibility = View.VISIBLE
     }
-    private fun hideData(){
+
+    private fun hideData() {
         binding.errorCard.visibility = View.GONE
         binding.inTempView.visibility = View.GONE
         binding.outTempView.visibility = View.GONE
         binding.relayView.visibility = View.GONE
     }
 
-    private fun showError(msg: String?){
+    private fun showError(msg: String?) {
         binding.errorMessage.text = msg
         binding.errorCard.visibility = View.VISIBLE
     }
@@ -115,7 +178,7 @@ class HomeFragment : Fragment() {
         binding.inHum.text = weather?.humid.toString() + " %"
     }
 
-    private fun setRelayStates(){
+    private fun setRelayStates() {
         binding.relay1State.text = states?.relayOne
         binding.relay2State.text = states?.relayTwo
         binding.relay3State.text = states?.relayThree
@@ -123,8 +186,10 @@ class HomeFragment : Fragment() {
     }
 
     private fun setHome() {
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(HomeController.appContext)
+        val sharedPreferences =
+            PreferenceManager.getDefaultSharedPreferences(HomeController.appContext)
         home = sharedPreferences.getString("home_signature", "").toString()
         binding.homeSign.text = home
     }
+
 }
